dab29f8ca60cfbb33170c62dd4c4c246bde91a2d
Manogna G <manogna.g06@gmail.com>
2026-07-20 12:20:38 +0530

    day1

---
+++ main.py
@@ -0,0 +1,49 @@
+#print("Manogna")
+#print("\\tcurrent\\new\\folder")# /  is escape character
+#print(r"\tcurrent\new\folder")
+#write a program to find odd and even with function
+#def odd_even(num):
+ #  if num%2==0:
+  #      print("Even")
+   # else:
+    #    print("odd")
+#odd_even(5)
+
+#dict={"name":"Manogna","gender":"female","age":20,"courses":["python","java","data science"]}
+#dict["name"]="Manu"
+#dict.update({"age":21})
+#print(dict)
+#print(dict.get("name"))
+#print(dict.values())
+
+#def count(*args):
+#    print(type(args))
+#count("manu manu")
+
+#def dicts(**kwargs):
+#    print(type(kwargs))
+#dicts(name="manogna",age=20,gender="female")
+
+#def default(gender,name="manogna",age=20):
+#    print(name,age,gender)
+#default("female")
+
+#OOPs
+# l=[1,2,3,4,5]
+# s="manogna"
+# print(len(l))
+# print(len(s))
+
+#take a string as input and 
+#print it back by removing the first and last character of the input string 
+# s="Manogna"
+# s=s[1:-1]
+# print(s[::-1])
+
+#take a number as input and find the sum of numbers fromm 1 to that number
+#x=int(input("Enter a number:"))
+# sum=0
+# for i in range(1,x+1):
+#     sum+=i
+# print(sum)
+
